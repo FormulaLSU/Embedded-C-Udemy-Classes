e9f1a41c9881b2333fede432755a272443b8b806
FormulaLSU <107905295+FormulaLSU@users.noreply.github.com>
2022-06-20 21:32:35 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+                                           // Use // to add a comment
+#include <stdio.h>
+
+int main()
+{                                          // Start with a curly bracket
+    printf("Hello World");                 // Displays a message in white plain text
+
+    return 0;                              // Displays an exit code
+}                                          // Start with a curly bracket
